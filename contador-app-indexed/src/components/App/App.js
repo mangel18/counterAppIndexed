fafe8445ebcf47703b36ccc42e5b822eb2744c9e
mangel18 <m_angel.18@hotmail.com>
2024-10-11 22:27:21 -0600

    base project

---
+++ contador-app-indexed/src/components/App/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import "antd/dist/reset.css";
+import ContadorForm from "../ContadorForm/contadorForm";
+
+function App() {
+  return (
+    <div className="App">
+      <ContadorForm />
+    </div>
+  );
+}
+
+export default App;
